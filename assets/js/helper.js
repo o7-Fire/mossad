33e4b69efa2fb1b014aeb9a3b76645404b75c35c
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-02-16 04:25:58 +0000

    brrrrr

---
+++ assets/js/helper.js
@@ -0,0 +1,8 @@
+console.log("H");
+let copyright = document.getElementById("copyright");
+let
+if(!copyright){
+    copyright.textContent = "o7-Fire© " + new Date().getFullYear();
+}
+//who cares if this crashed
+
